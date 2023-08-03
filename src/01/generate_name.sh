bc67e4f37e369df3dc2c800876932c8b4990452f
klotzgal <ggg44466635@gmail.com>
2023-08-03 10:00:27 +0000

    Начало генератора имен

--- src/01/generate_name.sh
+++ src/01/generate_name.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
 nameGen() {
-    str=$2
-
+    path=$1
+    number=$2
+    str=$3
 
     # парсим строку на массив уникальных букв
     for (( i=0; i < ${#str}; i++ )) do
         if !(echo ${arr[@]} | grep -qw "${str:$i:1}") then
             arr+=(${str:$i:1})
-            echo ${str:$i:1}
         fi
     done
     echo "|${arr[@]}|${#arr[@]}"
 
-    res=${arr[@]}
-    res=${res// /} # замена пробелов на ничто
+    res=""
+    # res=${res// /} # замена пробелов на ничто
     limit=$((249 - ${#arr[@]}))
-    pref=${arr[0]}
-    echo pref = $pref
+    
 # первый способ записи макс имени
-    for (( i=1; i < $limit; i++ )) do
-        res=${pref}a${res:${#pref}}
+    j=0
+    pref=""
+    letter=${arr[$j]}
+    echo pref = $pref
+    postf="${arr[@]}_$(date "+%d%m%y")"
+    postf=${postf// /}
+    postf=${postf:1}
+    count=0
+    while [ $count -lt $number ]; do
+        for (( i=1; i < $limit; i++ )) do
+            res=$letter$res
+            count=$(($count + 1))
+            if [ $count -ge $number ]; then
+                break
+            fi
+        done
+        echo $pref$res$postf
+        pref=$pref${arr[$j]}
+        j=$(($j + 1))
+        letter=${arr[$j]}
+        postf=${postf:1}
+        res=""
     done
-    last="_$(date "+%d%m%y")"
 
 
+    echo $count
 
-    res=$res"_$(date "+%d%m%y")"
-    mkdir $res
+    res=$pref$res$postf
+    # mkdir $res
 
 }
--- src/01/main.sh
+++ src/01/main.sh
@@ -7,9 +7,9 @@ if [ $? -eq 1 ]; then
     exit 1
 fi
 
-nameGen "xxx" $3
+nameGen $1 $2 $3
 
-echo $res
+# echo $res
 
 
 for (( i=1; i <= 4; i++ )) do
