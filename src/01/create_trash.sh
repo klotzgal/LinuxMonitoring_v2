f81153b62f3b28fb59356e5ed9291866def3566c
klotzgal <ggg44466635@gmail.com>
2023-08-05 12:05:12 +0000

    Остался лог

--- src/01/create_trash.sh
+++ src/01/create_trash.sh
@@ -5,21 +5,21 @@ folders_and_files() {
     all_folders=(${generated_names[@]})
     nameGen $a $4 $5 files
     all_files=(${generated_names[@]})
-    disk_size=$(df / | grep / | awk '{print $2}')
+    # disk_size=$(df / | grep / | awk '{print $2}')
     for folder in ${all_folders[@]} 
     do
-        disk_used=$(df / | grep / | awk '{print $3}')
-        if [ $(echo "$disk_size - $disk_used" | bc) -lt 1048576 ]; then
+        disk_space=$(df / | grep / | awk '{print $4}')
+        if [ $disk_space -lt 1048576 ]; then
             break
         fi
         mkdir $folder
         # echo $folder
         for file in ${all_files[@]} 
         do
-            disk_used=$(df / | grep / | awk '{print $3}')
-            if [ $(echo "$disk_size - $disk_used" | bc) -lt 1048576 ]; then
-                break
-            fi
+            # disk_used=$(df / | grep / | awk '{print $3}')
+            # if [ $(echo "$disk_size - $disk_used" | bc) -lt 1048576 ]; then
+            #     break
+            # fi
             # echo $folder/$file
             fallocate -l $6KB $folder/$file  
             
