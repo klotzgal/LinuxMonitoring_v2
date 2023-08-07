a2227d03bfae18e20201e72eec451da8b4f53789
klotzgal <ggg44466635@gmail.com>
2023-08-07 19:34:04 +0000

    Удалять по времени прикольно (Удалил проект)

--- src/03/delete_files.sh
+++ src/03/delete_files.sh
@@ -1,19 +1,86 @@
 #!/bin/bash
 
-
-
-del() {
-    # cat ../01/log.txt | awk '{print $1}' | xargs rm
+log_del() {
     all_filepath=($(cat ../02/log.txt | awk '{print $1}'))
+    cur_path=$(pwd)
+    del &> /dev/null
+    cd $cur_path
+    echo  "" > ../02/log.txt
+}
+
+mask_del() {
+    read -p "Введите маску:" mask
+    echo $mask _070823
+    all_filepath=($(find / -name "*$mask" 2>/dev/null))
+    del &> /dev/null
+}
+
+datetime_valid() {
+    local y="^[0-9]{4}$"
+    local m1="^0[1-9]$"
+    local m2="^1[0-2]$"
+    local d1="^0[1-9]$"
+    local d2="^[1-2][0-9]$"
+    local d3="^3[0-1]$"
+    local h1="^[0-1][0-9]$"
+    local h2="^2[0-3]$"
+    local MS="^[0-5][0-9]$"
+    # колдую временной массив
+    local str=${1//-/ }
+    str=${str//:/ }
+    str=($str)
     
+    if ((echo "${str[0]}" | grep -E -q -e $y ) && \
+        (echo "${str[1]}" | grep -E -q -e $m1 -e $m2 ) && \
+        (echo "${str[2]}" | grep -E -q -e $d1 -e $d2 -e $d3 ) && \
+        (echo "${str[3]}" | grep -E -q -e $h1 -e $h2 ) && \
+        (echo "${str[4]}" | grep -E -q -e $MS ) && \
+        (echo "${str[5]}" | grep -E -q -e $MS )) 
+    then
+        res=0
+    else
+        res=1
+    fi
+    return $res
+}
+
+time_del() {
+    echo "Введите дату и время в формате \"$(date '+%Y-%m-%d %H:%M:%S')\""
+    read -p "Время начала:" start_time
+    read -p "Время конца:" end_time
+    start_time="2023-08-06 11:25:49"
+    end_time="2023-08-07 18:25:49"
+
+    echo start $start_time
+    echo end $end_time
+    datetime_valid "$start_time"
+    if [ $? -eq 0 ]; then
+        datetime_valid "$end_time"
+        if [ $? -eq 0 ]; then
+            all_filepath=($(find / -newermt "$start_time" -not -newermt "$end_time" \
+             2>/dev/null))
+            echo ${all_filepath[@]} > a.txt
+
+            # del 
+        else
+            echo "Неверный формат даты конца"
+        fi
+    else
+        echo "Неверный формат даты начала"
+    fi
+
+}
+del() {    
+    # echo $all_filepath
     for file in ${all_filepath[@]}
-    do
+    do 
+        # if (echo "$file" | grep -E -q "/dev" ) then
         path=${file%/*} 
         cd ${file%/*} 
         rm -r ${file##*/}
         echo path
-        # echo $(ls)
         echo ${file##*/}
+        # fi
     done
 
 
--- src/03/main.sh
+++ src/03/main.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-# . ./validator.sh
+. ./validator.sh
 . ./delete_files.sh
 
-cur_path=$(pwd)
-del $1
-cd $cur_path
-echo  "" > ../02/log.txt
+# cur_path=$(pwd)
+# del $1
+# cd $cur_path
 
-# # valid $1 $2 $3
-# if [ $? -eq 0 ]; then
-#     folders_and_files $1 $2 $3
-# fi
+
+valid $1 $2 $3
+if [ $? -eq 0 ]; then
+    if [ $1 -eq 1 ]; then
+        echo log_del
+        log_del
+    elif [ $1 -eq 2 ]; then
+        echo time_del
+        time_del
+    elif [ $1 -eq 3 ]; then
+        echo mask_del
+        mask_del
+    fi
+fi
 
 
 
