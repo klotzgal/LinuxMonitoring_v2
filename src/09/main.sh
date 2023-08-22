055933c946e4de35c169ba79e50c7a9295a97000
klotzgal <ggg44466635@gmail.comm>
2023-08-22 10:10:05 +0000

    колдую скрипт

--- src/09/main.sh
+++ src/09/main.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 # . ./validator.sh
 
+HTML=/var/www/html/metrics/metrics.html
+
 while [ 1 -eq 1 ]
 do
-    CPU=$(lscpu | grep -e "CPU MHz" | awk '{print $3}')
-    curl -X POST -H  "Content-Type: text/plain" --data "$CPU" http://localhost:9090/metrics/
+    echo -n "" > $HTML
+    
+    CPU=$(lscpu | grep -e "CPU MHz" | awk '{print $3}' ) #| awk 'BEGIN {FS="."}{ print $1}')
+#     curl -X POST -H  "Content-Type: text/plain" --ipv4 --data "$CPU" http://localhost:12345/metrics
+    echo "<!DOCTYPE html><html><head>">> $HTML
+    echo "<title>My exporter</title></head><body>">> $HTML
+    echo "# HELP avail_cpu CPU stats<br>">> $HTML
+    echo "# TYPE avail_cpu counter<br>">> $HTML
+    echo "avail_cpu $CPU<br></body>">> $HTML
     sleep 3
-done
+done 
+
 
 
 
