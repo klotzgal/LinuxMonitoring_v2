58f946e3e5e1487f160748fe2bb112f41d0312d7
klotzgal <ggg44466635@gmail.comm>
2023-08-22 16:55:52 +0000

    Начинаю делать отчет по 9

--- src/09/main.sh
+++ src/09/main.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
-# . ./validator.sh
 
 HTML=/var/www/html/metrics/metrics.html
 
 while [ 1 -eq 1 ]
 do
-    echo -n "" > $HTML
-    
-    CPU=$(lscpu | grep -e "CPU MHz" | awk '{print $3}' ) #| awk 'BEGIN {FS="."}{ print $1}')
-#     curl -X POST -H  "Content-Type: text/plain" --ipv4 --data "$CPU" http://localhost:12345/metrics
-    echo "<!DOCTYPE html><html><head>">> $HTML
-    echo "<title>My exporter</title></head><body>">> $HTML
-    echo "# HELP avail_cpu CPU stats<br>">> $HTML
-    echo "# TYPE avail_cpu counter<br>">> $HTML
-    echo "avail_cpu $CPU<br></body>">> $HTML
+    CPU=$(lscpu | grep -e "CPU MHz" | awk '{print $3}' | awk 'BEGIN {FS="."}{ print $1}')
+    RAM=$(free | grep Mem | awk '{print $7}')
+    DISK=$(df / | grep / | awk '{print $4}')
+    DISK=${DISK/G/}
+    echo "# HELP avail_cpu CPU stats"> $HTML
+    echo "# TYPE avail_cpu gauge">> $HTML
+    echo "avail_cpu $CPU">> $HTML
+    echo "# HELP avail_ram available bytes RAM">> $HTML
+    echo "# TYPE avail_ram gauge">> $HTML
+    echo "avail_ram $RAM">> $HTML
+    echo "# HELP avail_disk_space bytes of available disk space">> $HTML
+    echo "# TYPE avail_disk_space gauge">> $HTML
+    echo "avail_disk_space $DISK">> $HTML
     sleep 3
-done 
-
-
-
-
-
-# valid $1 $2 $3
-# if [ $? -eq 0 ]; then
-# fi
-
+done
 
 
 
